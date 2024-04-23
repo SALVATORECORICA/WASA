5a479ae97a6f507c0e7b509210b99b735d3d92e2
CoricaS <salvatore.corica@gmd.de>
2024-04-23 10:10:03 +0200

    Iniziato il postSessionHandler

--- service/api/postSessionHandler.go
+++ service/api/postSessionHandler.go
@@ -1,1 +1,25 @@
 package api
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+// HTTP handler that checks the API server status. If the server cannot serve requests (e.g., some
+// resources are not ready), this should reply with HTTP Status 500. Otherwise, with HTTP Status 200
+// We have 3 input parameters, the first is the reply of the HTTP Request, the second one is the URL and Body request, the third one is the parameters of the URL Path
+func (rt *_router) postSessionHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+	//Check of the Server is ready:
+	if err := rt.DB.Ping(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Check of the HTTP method is POST
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+}
