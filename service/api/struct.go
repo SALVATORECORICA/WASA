1a6f532d87851e032c429ef86ac4e5c35cadfd7f
CoricaS <salvatore.corica@gmd.de>
2024-04-30 13:03:27 +0200

    update

--- service/api/struct.go
+++ service/api/struct.go
@@ -10,8 +10,35 @@ type DataId struct {
 	Id int `json:"id"`
 }
 
-// th complete Profile of the user
+// the Profile of the user
 type User struct {
 	Id       int    `json:"id"`
 	Nickname string `json:"nickname"`
+}
+
+//the comments
+type Comments struct {
+	Comment_id int    `json:"comment_id"`
+	Comment    string `json:"comment"`
+	photo_id   int    `json:"photo_id"`
+	User       User   `json:"user"`
+}
+
+// the photos
+
+type Photos struct {
+	Photo_id int       `json:"photo_Id"`
+	Owner    User      `json:"owner"`
+	date     Date      `json:"date"`
+	likes    []User    `json:"likes"`
+	comments []Comment `json:"comments"`
+}
+
+// the complete Profile of the user
+type User_Profile struct {
+	Id         int      `json:"id"`
+	Nickname   string   `json:"nickname"`
+	Followers  []User   `json:"followers"`
+	Followings []User   `json:"following"`
+	Photos     []Photos `json:"photos"`
 }
